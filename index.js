63340989ddcb89efd5cae38289ce3402b76db73b
R-R <73653265+wu-yu-pei@users.noreply.github.com>
2026-04-16 09:52:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const os = require("os")
+const fs = require('fs')
 
 const app = express();
+fs.mkdirSync('./file', { recursive: true })
 
 let time = 0;
 
 setInterval(() => {
-  time++
+  fs.writeFileSync('./file/a.txt', '--', {
+    flag: 'a',
+    encoding: 'utf-8',
+  })
 }, 1000)
 
 app.get('/', (req, res) => {
